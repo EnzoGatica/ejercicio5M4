a5c37ec9a8cfa4ad42b0b43419839c1bef17b051
Enzo_Gatica <enzogatica96@gmail.com>
2023-06-19 20:33:10 -0400

    Primer Commit, Layout Creado

---
+++ app/src/main/java/com/example/ejercicio5/MainActivity.java
@@ -0,0 +1,32 @@
+package com.example.ejercicio5;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    private TextView textresultado;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        textresultado = findViewById(R.id.textresultado);
+        Button btntoast = findViewById(R.id.btntoast);
+        Button btncound = findViewById(R.id.btntoast);
+
+        btntoast.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(getBaseContext(), "Apretaste Toast", Toast.LENGTH_LONG).show();
+            }
+        });
+
+    }
+}
